c384dbc49bbf528062935d66ae6a8f2f9dc46442
AfiaAkterMunni <afiamunni03@gmail.com>
2024-08-21 14:39:17 +0600

    store contact

--- laravel/app/Http/Controllers/ContactController.php
+++ laravel/app/Http/Controllers/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreContactRequest;
+use App\Models\Contact;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
@@ -14,9 +15,20 @@ class ContactController extends Controller
 
     public function store(StoreContactRequest $request)
     {
-        dd('fhfh');
-        dd($request);
-        return view('pages.contact');
+        $image = $request->file('image');
+        $name = time().'.'.$image->getClientOriginalExtension();
+        $destinationPath = public_path('/uploads/contacts');
+        $image->move($destinationPath, $name);
+        $data = [
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'phone' => $request->input('phone'),
+            'message' => $request->input('message'),
+            'image' => $name
+        ];
+        Contact::create($data);
+        return redirect(route('contact'))->with('success', 'Your Message Sent Successfully!!!');
+        
     }
 
 
--- laravel/resources/views/pages/contact.blade.php
+++ laravel/resources/views/pages/contact.blade.php
@@ -48,12 +48,29 @@
         <h1 class="text-3xl lg:text-2xl font-semibold tracking-wider">Send Us Your Requirement</h1>
         <div class="bg-white rounded-lg">
             <div class="py-6">
+                @if (session('success'))
+                <div class="flex items-center justify-between py-2 px-4 mb-8 text-md text-white bg-green-600 rounded-lg shadow-md focus:outline-none focus:shadow-outline-purple"
+                    id="toaster">
+                    <div class="flex items-center">
+                        <span>{{ session('success') }}</span>
+                    </div>
+                    <button
+                        class="inline-flex items-center justify-center w-6 h-6 text-white transition-colors duration-150 rounded dark:hover:text-gray-200 hover: hover:text-gray-700"
+                        aria-label="close" onclick="document.getElementById('toaster').style.display = 'none';">
+                        <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" role="img" aria-hidden="true">
+                            <path
+                                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                clip-rule="evenodd" fill-rule="evenodd"></path>
+                        </svg>
+                    </button>
+                </div>
+                @endif
                 <form action="{{route('contact.store')}}" method="POST" enctype="multipart/form-data">
                     @csrf
                     <div class="mb-1">
                         <input name="name" type="text" placeholder="Name" class="h-12 px-3 mb-4 bg-gray-50 w-full border-gray-300 border rounded focus:outline-none focus:border-cyan-50">
                         @error('name')
-                            <p class="text-red-500">{{$message}}</p>
+                        <p class="text-red-500">{{$message}}</p>
                         @enderror
                     </div>
 
@@ -61,23 +78,23 @@
                         <input name="email" type="email" placeholder="Email"
                             class="h-12 px-3 mb-4 bg-gray-50 w-full border-gray-300 border rounded focus:outline-none focus:border-cyan-50">
                         @error('email')
-                            <p class="text-red-500">{{$message}}</p>
+                        <p class="text-red-500">{{$message}}</p>
                         @enderror
                     </div>
 
                     <div class="mb-1">
                         <input name="phone" type="text" placeholder="Phone Number" class="h-12 px-3 mb-4 bg-gray-50 w-full border-gray-300 border rounded focus:outline-none focus:border-cyan-50">
                         @error('phone')
-                            <p class="text-red-500">{{$message}}</p>
+                        <p class="text-red-500">{{$message}}</p>
                         @enderror
                     </div>
 
 
                     <div class="mb-1">
                         <textarea name="message" type="text" placeholder="Message"
-                        class="h-24 py-1 px-3 mb-4 bg-gray-50 w-full border border-gray-300 rounded focus:outline-none focus:border-cyan-50"></textarea>
+                            class="h-24 py-1 px-3 mb-4 bg-gray-50 w-full border border-gray-300 rounded focus:outline-none focus:border-cyan-50"></textarea>
                         @error('message')
-                            <p class="text-red-500">{{$message}}</p>
+                        <p class="text-red-500">{{$message}}</p>
                         @enderror
                     </div>
 
@@ -85,7 +102,7 @@
                         <input type="file" name="image"
                             class="h-10 px-3 mb-4 bg-gray-50 w-full border border-gray-300 rounded focus:outline-none focus:border-cyan-50">
                         @error('image')
-                            <p class="text-red-500">{{$message}}</p>
+                        <p class="text-red-500">{{$message}}</p>
                         @enderror
                     </div>
 
@@ -99,9 +116,9 @@
 </div>
 <!-- Contact End-->
 
-  <!-- scroll on top button Start-->
-  <a id="button" class="inline-block bg-blue-600 w-9 h-9 text-center rounded fixed bottom-5 right-5 opacity-0 invisible z-[1000] after:content-['\f077'] font-['FontAwesome'] text-2xl text-white hover:cursor-pointer hover:bg-gray-500 active:bg-sky-400"></a>
-  <!-- scroll on top button End-->
+<!-- scroll on top button Start-->
+<a id="button" class="inline-block bg-blue-600 w-9 h-9 text-center rounded fixed bottom-5 right-5 opacity-0 invisible z-[1000] after:content-['\f077'] font-['FontAwesome'] text-2xl text-white hover:cursor-pointer hover:bg-gray-500 active:bg-sky-400"></a>
+<!-- scroll on top button End-->
 
 <script type="text/javascript" src="{{asset('js/frontend/menu.js')}}"></script>
 <script type="text/javascript" src="{{asset('js/frontend/scrollbutton.js')}}"></script>
