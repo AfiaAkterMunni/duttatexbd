2001c2057c012be42413ca8f70bf279e364b35f6
afiaaktermunni <afiamunni03@gmail.com>
2025-03-01 17:38:46 +0600

    chore(nav): frontend navbar active design.

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -9,19 +9,19 @@
       </span>
     </div>
     <ul class="md:flex pr-4 md:text-white text-black w-full md:w-auto md:static absolute top-[-400px] left-0 transition-all ease-in duration-500 z-[5] md:z-auto md:py-0 py-4 bg-[#027DAE]">
-      <li class="py-3 px-4">
+      <li class="py-3 px-4 {{ (strcmp(Route::currentRouteName(), 'homepage') == 0) ? 'text-cyan-200' : '' }}">
         <a href="{{route('homepage')}}" class="text-lg hover:text-cyan-200">Home</a>
       </li>
-      <li class="py-3 px-4">
+      <li class="py-3 px-4 {{ (strcmp(Route::currentRouteName(), 'about') == 0) ? 'text-cyan-200' : '' }}">
         <a href="{{route('about')}}" class="text-lg hover:text-cyan-200">About</a>
       </li>
-      <li class="py-3 px-4">
+      <li class="py-3 px-4 {{ (strcmp(Route::currentRouteName(), 'category') == 0) ? 'text-cyan-200' : '' }}">
         <a href="{{route('category')}}" class="text-lg hover:text-cyan-200">Products</a>
       </li>
-      <li class="py-3 px-4">
+      <li class="py-3 px-4 {{ (strcmp(Route::currentRouteName(), 'service') == 0) ? 'text-cyan-200' : '' }}">
         <a href="{{route('service')}}" class="text-lg hover:text-cyan-200">Services</a>
       </li>
-      <li class="py-3 px-4">
+      <li class="py-3 px-4 {{ (strcmp(Route::currentRouteName(), 'contact') == 0) ? 'text-cyan-200' : '' }}">
         <a href="{{route('contact')}}" class="text-lg hover:text-cyan-200">Contact</a>
       </li>
     </ul>
