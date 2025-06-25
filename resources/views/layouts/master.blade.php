75641e319e4015ffb8cc88378bb1140b5823377c
AfiaAkterMunni <afiamunni03@gmail.com>
2025-06-25 18:43:33 +0600

    modify name

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,37 +1,40 @@
 <!DOCTYPE html>
 <html class="no-js" lang="en-US">
-    <head>
-        <link rel="icon" type="image/x-icon" href="{{asset('images/fav-icon.jpg')}}">
-        <title>@yield('title')</title>
-        @include('includes.head')
-        {{-- @dump(isset($jsonLD)) --}}
-        @if(isset($jsonLD))
-            <!-- JSON-LD -->
-            <script type="application/ld+json">
-                {!! json_encode($jsonLD) !!}
-            </script>
-        @endif
-    </head>
 
-    <body>
+<head>
+    <link rel="icon" type="image/x-icon" href="{{asset('images/nrbFashion.png')}}">
+    <title>@yield('title')</title>
+    @include('includes.head')
+    {{-- @dump(isset($jsonLD)) --}}
+    @if(isset($jsonLD))
+    <!-- JSON-LD -->
+    <script type="application/ld+json">
+        {
+            !!json_encode($jsonLD) !!
+        }
+    </script>
+    @endif
+</head>
 
-        @include('includes.navbar')
+<body>
 
-        @isset($seo)
-            @if (!empty($seo->h1_text))
-                <h1 style='font-size: 0px;line-height: 0px;margin: 0;padding:0'>{{ $seo->h1_text }}</h1>
-            @endif
-        @endisset
+    @include('includes.navbar')
 
-        @yield('content')
+    @isset($seo)
+    @if (!empty($seo->h1_text))
+    <h1 style='font-size: 0px;line-height: 0px;margin: 0;padding:0'>{{ $seo->h1_text }}</h1>
+    @endif
+    @endisset
+
+    @yield('content')
 
 
-        @include('includes.footer')
+    @include('includes.footer')
 
 
-        <!-- cdn link for ionicons icon -->
-        <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-        <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-    </body>
+    <!-- cdn link for ionicons icon -->
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
 
 </html>
